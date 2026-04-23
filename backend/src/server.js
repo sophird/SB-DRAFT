297d0c0d822d6c013b9460f112d3a6d5560dc110
Sophia Rainne O. Dumayas <202410699@gordoncollege.edu.ph>
2026-04-23 23:46:31 +0800

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -46,8 +46,8 @@ app.use(
 app.use(express.json());
 
 const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL || "",
-  process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY || ""
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 const supabaseAuth = createClient(
   process.env.SUPABASE_URL || "",
